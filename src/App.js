8d32b67b755a7d7b97f6c87febaadf169f48286a
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-16 08:46:10 -0500

    implement component for todos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+import TodoList from './components/todos/TodoList';
+
 import './App.css';
 
 const App = () => {
   return (
     <div className='App'>
       <h2>Hello, world</h2>
+      <TodoList />
     </div>
   )
 };
